0b404b6f843a4195ee6d4ad7dab83e81dfdcbbb1
Kevin Li <kmli@mit.edu>
2018-02-17 13:21:34 -0500

    finish sabato scraping

--- scrapers/sabato.py
+++ scrapers/sabato.py
@@ -2,31 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-def scrape_sheet(outfile_name, gid, state_list):
-    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT4KY7cdB2oiA8_Qlfjuei0O9LkApgXMnyUm4pcW2KR5pKsTRcEcnC-UoSB8LfqljT1ktFI5e9CPJUn/pubhtml?gid=' + gid
+def scrape_sheet(outfile_name, gid, state_list, num_cols):
+    print(outfile_name)
+    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT4KY7cdB2oiA8_Qlfjuei0O9LkApgXMnyUm4pcW2KR5pKsTRcEcnC-UoSB8LfqljT1ktFI5e9CPJUn/pubhtml?gid=' + gid + '&single=true'
     soup = BeautifulSoup(requests.get(url).content, 'html.parser')
 
     output = []
     for state in state_list:
-        state_row = soup.find('td', text=state)
-        if state_row is None:
+        state_rows = soup('td', text=state)
+        if not state_rows:
             continue
-        contents = [x.text for x in state_row.parent.findChildren()[2:8]]
-        output.append(contents)
+        for state_row in state_rows:
+            contents = [x.text for x in state_row.parent.findChildren()[2:2 + num_cols]]
+            if contents[1] == 'AL':
+                contents[1] == '1'
+            if contents not in output:
+                output.append(contents)
 
     with open('sabato_' + outfile_name + '.csv','w+') as my_csv:
         csvWriter = csv.writer(my_csv, delimiter=',')
         csvWriter.writerows(output)
 
-def senators():
-    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT4KY7cdB2oiA8_Qlfjuei0O9LkApgXMnyUm4pcW2KR5pKsTRcEcnC-UoSB8LfqljT1ktFI5e9CPJUn/pubhtml'
-
-
 senate_gid = '0'
 governor_gid = '1856131767'
+house_gid = '2083419594'
 with open('states.txt') as fin:
     state_names = [x.strip() for x in fin.readlines()]
 
+with open('state_abbrs.txt') as fin:
+    state_abbrs = [x.strip() for x in fin.readlines()]
+
 state_names = state_names + ['Minnesota (S)']
 
-governors()
+scrape_sheet('senate', senate_gid, state_names, 6)
+scrape_sheet('governors', governor_gid, state_names, 6)
+scrape_sheet('house', house_gid, state_abbrs, 7)
