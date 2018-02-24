32f0d1dce471d67bb872b80711b0867ead53e309
Kevin Li <kmli@mit.edu>
2018-02-24 15:02:32 -0500

    initial shell for zipform

--- student-voting/src/components/ZipForm.js
+++ student-voting/src/components/ZipForm.js
@@ -3,9 +3,51 @@ import React, { Component } from 'react';
 class ZipForm extends Component {
   // props: onSelectZip (callback, takes zip)
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      home: {
+        name: 'home',
+        zip: ''
+      },
+      school: {
+        name: 'school',
+        zip: ''
+      },
+    };
+  }
+
+  renderTextBox(zipType) {
+    return (
+      <input
+        type="text"
+        value={this.state[zipType].zip}
+        placeholder={formatLabel(zipType) + " ZIP"}
+      />
+    )
+  }
+
+  formatLabel (zipType) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  compare () {
+    
+  }
+
+  validateZip () {
+
+  }
+
   render() {
     return (
-      <div>YO I'M A ZIPFORM</div>
+      <div>
+        {this.renderTextBox("home")}
+        {this.renderTextBox("school")}
+        <button onClick={this.compare}>
+          Compare
+        </button>
+      </div>
     );
     // display input text boxes and possibly ask for address
   }
