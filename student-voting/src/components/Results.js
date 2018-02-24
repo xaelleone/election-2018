8f4e76a2366c8ca0ed44633bf938588f30c9e8a5
Victoria Xia <vxia@mit.edu>
2018-02-24 10:48:23 -0500

    create app shell.

---
+++ student-voting/src/components/Results.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Results extends Component {
+  // props: 
+
+  render() {
+    return null;
+    // display results showing
+  }
+}
+
+export default Results;
---
+++ student-voting/src/components/ZipForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ZipForm extends Component {
+  // props: onSelectZip (callback, takes zip)
+
+  render() {
+    return (
+      <div>YO I'M A ZIPFORM</div>
+    );
+    // display input text boxes and possibly ask for address
+  }
+}
+
+export default ZipForm;
